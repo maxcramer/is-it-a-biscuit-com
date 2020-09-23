3445e2763a271a9e5019fa35440f6c55d56e722a
maxcramer <maximiliancramer@yahoo.com>
2020-09-23 14:45:53 +0100

    working on searchbar passing data

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,34 @@
 import React, { useState, useEffect, createRef } from 'react';
 
+import {getBiscuits} from '../../services/firestoreService';
 
 function SearchBar() {
-    const [biscuit, setBiscuit] = useState([]);
+    const [biscuits, setBiscuit] = useState([]);
+    const [results, setResults] = useState([]);
     const searchInput = createRef();
 
     const filterResults = () => {
         const searchTerm = searchInput.current.value;
-        // const biscuit = 
+        const biscuitResults = biscuits.filter(biscuit => biscuit.name.toLowerCase().includes(searchTerm.toLowerCase()));
+        setResults([biscuitResults]);
+        console.log(biscuitResults);
     }
 
     useEffect(() => {
 
-    })
+        const fetchBiscuits = async () => {
+            const biscuitResponse = await getBiscuits();
+            setBiscuit(biscuitResponse);
+        }
+
+        const loadData = async () => {
+            await Promise.all([fetchBiscuits()]);
+            setResults([])
+        }
+        loadData();
+        setResults([]);
+
+    }, [])
 
     return (
         <div>
--- src/services/firestoreService.js
+++ src/services/firestoreService.js
@@ -29,4 +29,4 @@ export const getBiscuits = async () => {
         }
     })
     return biscuits
-}
+};
