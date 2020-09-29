6032d21a3efb6cfd7eaa47b14e54485983b6bf42
maxcramer <maximiliancramer@yahoo.com>
2020-09-29 13:18:00 +0100

    urls changing on click and not adding parts on, data still not rendering

--- src/components/Results/BiscuitRoutes.jsx
+++ src/components/Results/BiscuitRoutes.jsx
@@ -5,7 +5,7 @@ import BiscuitResults from './BiscuitResults';
 
 const BiscuitList = () => (
     <Switch>
-        <Route path='/biscuitResults/:id' component={BiscuitResults} />
+        <Route path='/biscuitList/:id' component={BiscuitResults} />
     </Switch>
 )
 
