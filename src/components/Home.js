cb0847206a0c90a8a1e2d632dc85d32dabb4fb5d
maxcramer <maximiliancramer@yahoo.com>
2020-09-30 11:57:41 +0100

    created CSS file for home and added container with display flex

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,16 +5,21 @@ import IsItABiscuitLogo from '../images/IsItABiscuitLogo.png'
 import SearchBar from './SearchBar/SearchBar';
 import BiscuitRoutes from './Results/BiscuitRoutes';
 
+import "./Home.css";
+
+
 function Home() {
     return(
         <Router>
-            <img src={IsItABiscuitLogo} alt="Logo" />
-            <h1>Is It A Biscuit?</h1>
-            <h3>Search below to find out!</h3>
-            <SearchBar />
-            <Switch>
-                <Route path="/biscuitResults" component={BiscuitRoutes} />
-            </Switch>
+            <div id="home_container">
+                <img src={IsItABiscuitLogo} alt="Logo" />
+                <h1>Is It A Biscuit?</h1>
+                <h3>Search below to find out!</h3>
+                <SearchBar />
+                <Switch>
+                    <Route path="/biscuitResults" component={BiscuitRoutes} />
+                </Switch>
+            </div>
         </Router>
     )
 }
