269ffc677054eda59f228f5eb74aef9880d78a46
maxcramer <maximiliancramer@yahoo.com>
2020-09-30 13:15:30 +0100

    adding reset button, app resets when click on logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,9 @@ function Home() {
     return(
         <Router>
             <div id="home_container">
-                <img id="logo" src={IsItABiscuitLogo} alt="Logo"  />
+                <a href="/">
+                    <img id="logo" src={IsItABiscuitLogo} alt="Logo"  />
+                </a>
                 <h1 id="title">Is It A Biscuit?</h1>
                 <h3 id="subtitle">Search below to find out!</h3>
                 <SearchBar />
