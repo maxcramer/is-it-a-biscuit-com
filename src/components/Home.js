100869720fcb75abd4dd1451e50db79899f40242
maxcramer <maximiliancramer@yahoo.com>
2020-10-21 13:09:27 +0100

    removing styling, setting up for redesign

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,11 +12,11 @@ function Home() {
     return(
         <Router>
             <div id="home_container">
-                <a href="/">
+                {/* <a href="/">
                     <img id="logo" src={IsItABiscuitLogo} alt="Logo"  />
-                </a>
-                <h1 id="title">Is It A Biscuit?</h1>
-                <h3 id="subtitle">Search below to find out!</h3>
+                </a> */}
+                {/* <h1 id="title">Is It A Biscuit?</h1>
+                <h3 id="subtitle">Search below to find out!</h3> */}
                 <SearchBar />
                 <Switch>
                     <Route path="/biscuitResults" component={BiscuitRoutes} />
