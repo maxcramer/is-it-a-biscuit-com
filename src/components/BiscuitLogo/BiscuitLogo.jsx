8644caeca53c60f857957fa50a38af953983de49
maxcramer <maximiliancramer@yahoo.com>
2020-11-02 15:28:40 +0000

    creating new component for on site load so initial load page isn't blank

---
+++ src/components/BiscuitLogo/BiscuitLogo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import IsItABiscuitLogo from '../../images/IsItABiscuitLogo.png'
+
+function BiscuitLogo () {
+    return (
+        <img src={IsItABiscuitLogo} alt=""/>
+    )
+}
+
+export default BiscuitLogo;
