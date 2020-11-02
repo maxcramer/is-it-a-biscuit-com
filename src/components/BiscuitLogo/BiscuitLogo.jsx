3c78fe6dbfecc8c3b3823a5341548c5c43fad7b9
maxcramer <maximiliancramer@yahoo.com>
2020-11-02 15:36:22 +0000

    adding title text to biscuit logo and removing commented out code from home page

--- src/components/BiscuitLogo/BiscuitLogo.jsx
+++ src/components/BiscuitLogo/BiscuitLogo.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import IsItABiscuitLogo from '../../images/IsItABiscuitLogo.png'
+
+import IsItABiscuitLogo from '../../images/IsItABiscuitLogo.png';
+import './BiscuitLogo.css';
 
 function BiscuitLogo () {
     return (
-        <img src={IsItABiscuitLogo} alt=""/>
+        <div>
+            <h1>Is It A Biscuit?</h1>
+            <h3>Search above to find out!</h3>
+            <img src={IsItABiscuitLogo} alt=""/>
+            <h5>*No, Jaffa Cakes are not biscuits! <a href="https://www.kerseys.co.uk/jaffa-cakes-cakes-biscuits/#:~:text=During%20the%20court%20battle%20between,recognised%20as%20chocolate%20covered%20cakes.">Click Here</a> to find out why</h5>
+        </div>
     )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import IsItABiscuitLogo from '../images/IsItABiscuitLogo.png'
 
 import SearchBar from './SearchBar/SearchBar';
 import BiscuitRoutes from './Results/BiscuitRoutes';
@@ -13,14 +12,7 @@ function Home() {
     return(
         <Router>
             <div id="home_container">
-                {/* <a href="/">
-                    
-                </a> */}
-                {/* <h1 id="title">Is It A Biscuit?</h1>
-                <h3 id="subtitle">Search below to find out!</h3> */}
                 <SearchBar />
-                {/* <img id="logo" src={IsItABiscuitLogo} alt="Logo"  /> */}
-
                 <Switch>
                     <Route path="/" exact component={BiscuitLogo} />
                     <Route path="/biscuitResults" component={BiscuitRoutes} />
